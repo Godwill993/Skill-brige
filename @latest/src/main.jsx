ad2b9f893a059da1233f256a77c6602d63576a3d
Godwill993 <lendzelegoodwill@gmail.com>
2025-11-12 00:03:42 +0100

    hello

--- @latest/src/main.jsx
+++ @latest/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
+import Navi from "./navi.jsx";
 import LandingPage from "./landigPage.jsx";
 import FeaturesSection from "./card/cardprop.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Navi />
     <LandingPage />
     <FeaturesSection />
   </StrictMode>
---
+++ @latest/src/navi.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "/src/App.css";
+import { FaHome, FaInfoCircle, FaServicestack, FaEnvelope, } from "react-icons/fa";
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (<>
+    <nav className="navbar">
+      <div className="nav-logo">Skill <span className="logo-style"> Bridge</span>  </div>
+
+      <div className={`nav-links ${isOpen ? "open" : ""}`}>
+        <a href="#home" onClick={() => setIsOpen(false)}><FaHome /> Home</a>
+        <a href="#about" onClick={() => setIsOpen(false)}><FaInfoCircle /> About</a>
+        <a href="#services" onClick={() => setIsOpen(false)}><FaServicestack /> Services</a>
+        <a href="#contact" onClick={() => setIsOpen(false)}><FaEnvelope /> Contact</a>
+      </div>
+
+      <div className="nav-toggle" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </nav>
+        </>
+  );
+};
+
+export default Navbar;
