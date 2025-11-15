2842edde0203356d3d6edb6a8b2e69aa5437a09e
Godwill993 <lendzelegoodwill@gmail.com>
2025-11-15 15:26:57 +0100

    this is the last thing i do today

--- @latest/src/pages/App.jsx
+++ @latest/src/pages/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "/src/App.css";
+import "../styles/App.css";
 
 import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import Main from  "../main";
