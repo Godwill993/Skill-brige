0a5c76a790711fccdeda061b6b0747a8d60dad81
Godwill993 <lendzelegoodwill@gmail.com>
2025-11-15 22:08:59 +0100

    trying to resolve importational issues

--- @latest/src/pages/App.jsx
+++ @latest/src/pages/App.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import "../styles/App.css";
 
 import { Routes, Route} from "react-router-dom";
-import {Login } from "./Login"
-import { Form } from "./form"
-import { LandingPage } from "../components/landigPage"
+import { Form } from "./form.jsx"
+import { LandingPage } from "../components/landigPage.jsx"
 function App() {
 
   return (
  
   <Routes>
-     <Route path="/" element={<Login/>} />
+     <Route path="/" element={<Form/>} />
      <Route path="/form" element={<Form/>} />  
      <Route path="/landingPage"/>
    </Routes>
--- @latest/src/pages/Login.jsx
+++
@@ -1,8 +0,0 @@
-function Form(){
-    return(
-        <div className="form">
-            <h1>hello</h1>
-        </div>
-    )
-}
-export default Form
