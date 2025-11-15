eb714db5732ddf41c9fc9c81b7a83cd69d40d59a
Godwill993 <lendzelegoodwill@gmail.com>
2025-11-15 15:30:20 +0100

    back to initial reduct

--- @latest/src/main.jsx
+++ @latest/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Navi from "./navi.jsx";
+import Navi from "./components/navi.jsx";
 import LandingPage from "./components/landigPage.jsx";
 import FeaturesSection from "./components/cardprop.jsx";
 createRoot(document.getElementById("root")).render(
