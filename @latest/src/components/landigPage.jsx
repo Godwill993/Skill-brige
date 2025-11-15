607e96bbcd925a2288bc8d6d7644793983071ec8
Godwill993 <lendzelegoodwill@gmail.com>
2025-11-15 13:43:16 +0100

    trying to organise the work

--- @latest/src/components/landigPage.jsx
+++ @latest/src/components/landigPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "/src/landingPage.css";
+import "/src/style/landingPage.css";
 import SignupForm from "../pages/form.jsx";
 import {HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { Link } from "react-router-dom"
