8cee6e640e6103a61f3f4ff347a8ef9471692ade
Godwill993 <lendzelegoodwill@gmail.com>
2025-11-17 10:25:11 +0100

    routes implimented

--- @latest/src/pages/App.jsx
+++ @latest/src/pages/App.jsx
@@ -11,7 +11,7 @@ function App() {
   <Routes>
      <Route path="/" element={<Form/>} />
      <Route path="/form" element={<Form/>} />  
-     <Route path="/landingPage"/>
+     <Route path="/welcome " element={<Welcome/>}/>
    </Routes>
   
 
