88c8bd86ada447455b24966bf6f7e3b21167a4c2
Godwill993 <lendzelegoodwill@gmail.com>
2025-11-17 12:07:20 +0100

    other import issues

--- @latest/src/pages/welcom.jsx
+++ @latest/src/pages/welcom.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Nav from "../components/navi.jsx";
 import LandingPage from "../components/landigPage.jsx";
-import "./index.css";
+import "../index.css";
 import  App  from "./pages/App";
  function  Welcome () {
   return (
