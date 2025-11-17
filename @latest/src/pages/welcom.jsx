6e9c1c4ae8ef7c4833291ce05fb9a95e4dc49aa0
Godwill993 <lendzelegoodwill@gmail.com>
2025-11-17 12:00:21 +0100

    this is correct the import error

--- @latest/src/pages/welcom.jsx
+++ @latest/src/pages/welcom.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Nav from "./components/navi.jsx";
+import Nav from "../components/navi.jsx";
 import LandingPage from "../components/landigPage.jsx";
 import "./index.css";
 import  App  from "./pages/App";
