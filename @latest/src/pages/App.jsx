b6d75f7602c12f4048d85b5879594261af702a53
Godwill993 <lendzelegoodwill@gmail.com>
2025-11-17 10:20:59 +0100

    welcome page creation and posting on routes

--- @latest/src/pages/App.jsx
+++ @latest/src/pages/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "../styles/App.css";
-
+import Welcome from "./welcom.jsx";
 import { Routes, Route} from "react-router-dom";
-import Form  from "./form.jsx"
-import  LandingPage  from "../components/landigPage.jsx"
+import Form  from "./form.jsx";
+import  LandingPage  from "../components/landigPage.jsx";
 function App() {
 
   return (
---
+++ @latest/src/pages/welcom.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Nav from "./components/navi.jsx";
+import LandingPage from "../components/landigPage.jsx";
+import "./index.css";
+import  App  from "./pages/App";
+ function  Welcome () {
+  return (
+  <>
+    <Nav></Nav>
+    <LandingPage></LandingPage>
+ </>
+  );
+}
+export default Welcome;
